91c5a0fd05aa35edbc190490646f5d627677f803
Remco Tolsma <info@remcotolsma.nl>
2014-08-20 10:15:45 +0200

    Improved the listener check, if one check fails it ends. Also adjusted the short var $id to $transation_id (phpmd).

--- src/Listener.php
+++ src/Listener.php
@@ -10,17 +10,18 @@
  */
 class Pronamic_WP_Pay_Gateways_Mollie_Listener implements Pronamic_Pay_Gateways_ListenerInterface {
 	public static function listen() {
-		$condition  = true;
-		$condition &= filter_has_var( INPUT_GET, 'mollie_webhook' );
-		$condition &= filter_has_var( INPUT_GET, 'type' );
-		$condition &= filter_has_var( INPUT_GET, 'id' );
-
-		if ( $condition ) {
-			$type = filter_input( INPUT_GET, 'type', FILTER_SANITIZE_STRING );
-			$id   = filter_input( INPUT_GET, 'id', FILTER_SANITIZE_STRING );
+		if ( 
+			filter_has_var( INPUT_GET, 'mollie_webhook' )
+				&&
+			filter_has_var( INPUT_GET, 'type' )
+				&&
+			filter_has_var( INPUT_GET, 'id' )
+		) {
+			$type           = filter_input( INPUT_GET, 'type', FILTER_SANITIZE_STRING );
+			$transaction_id = filter_input( INPUT_GET, 'id', FILTER_SANITIZE_STRING );
 
 			if ( 'payment' == $type ) {
-				$payment = get_pronamic_payment_by_transaction_id( $id );
+				$payment = get_pronamic_payment_by_transaction_id( $transaction_id );
 
 				Pronamic_WP_Pay_Plugin::update_payment( $payment );
 			}
