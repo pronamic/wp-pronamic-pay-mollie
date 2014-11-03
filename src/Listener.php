c88bf6e443ee55e74e5f0d0e21f22e871b6af813
Remco Tolsma <info@remcotolsma.nl>
2014-11-03 16:32:52 +0100

    Adjusted the Mollie listener to the Mollie API documentation. https://www.mollie.nl/files/documentatie/payments-api.html

--- src/Listener.php
+++ src/Listener.php
@@ -10,21 +10,16 @@
  */
 class Pronamic_WP_Pay_Gateways_Mollie_Listener implements Pronamic_Pay_Gateways_ListenerInterface {
 	public static function listen() {
-		if ( 
+		if (
 			filter_has_var( INPUT_GET, 'mollie_webhook' )
 				&&
-			filter_has_var( INPUT_GET, 'type' )
-				&&
-			filter_has_var( INPUT_GET, 'id' )
+			filter_has_var( INPUT_POST, 'id' )
 		) {
-			$type           = filter_input( INPUT_GET, 'type', FILTER_SANITIZE_STRING );
-			$transaction_id = filter_input( INPUT_GET, 'id', FILTER_SANITIZE_STRING );
+			$transaction_id = filter_input( INPUT_POST, 'id', FILTER_SANITIZE_STRING );
 
-			if ( 'payment' == $type ) {
-				$payment = get_pronamic_payment_by_transaction_id( $transaction_id );
+			$payment = get_pronamic_payment_by_transaction_id( $transaction_id );
 
-				Pronamic_WP_Pay_Plugin::update_payment( $payment );
-			}
+			Pronamic_WP_Pay_Plugin::update_payment( $payment );
 		}
 	}
 }
