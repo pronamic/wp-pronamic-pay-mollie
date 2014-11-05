060d2f08d1064b0283ef630afa55208765649fbe
Remco Tolsma <info@remcotolsma.nl>
2014-11-05 14:50:40 +0100

    Updated documentation.

--- src/Statuses.php
+++ src/Statuses.php
@@ -3,11 +3,10 @@
 /**
  * Title: Mollie statuses constants
  * Description:
- * Copyright: Copyright (c) 2005 - 2011
+ * Copyright: Copyright (c) 2005 - 2014
  * Company: Pronamic
  * @author Remco Tolsma
- * @version 1.0
- * @see https://www.mollie.nl/support/documentatie/betaaldiensten/ideal/en/
+ * @version 1.0.0
  */
 class Pronamic_WP_Pay_Mollie_Statuses {
 	/**
