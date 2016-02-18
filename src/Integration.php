3173b5daa0956638cd4254ba0aadddbd637ad711
Remco Tolsma <info@remcotolsma.nl>
2016-02-18 13:17:07 +0100

    Added get_settings function for the integration class.

--- src/Integration.php
+++ src/Integration.php
@@ -7,7 +7,8 @@
  * Company: Pronamic
  *
  * @author Remco Tolsma
- * @version 1.0.0
+ * @version 1.1.4
+ * @since 1.0.0
  */
 class Pronamic_WP_Pay_Gateways_Mollie_Integration extends Pronamic_WP_Pay_Gateways_AbstractIntegration {
 	public function __construct() {
@@ -38,5 +39,20 @@ class Pronamic_WP_Pay_Gateways_Mollie_Integration extends Pronamic_WP_Pay_Gatewa
 
 	public function get_gateway_class() {
 		return 'Pronamic_WP_Pay_Gateways_Mollie_Gateway';
+	}
+
+	/**
+	 * Get required settings for this integration.
+	 *
+	 * @see https://github.com/wp-premium/gravityforms/blob/1.9.16/includes/fields/class-gf-field-multiselect.php#L21-L42
+	 * @since 1.1.3
+	 * @return array
+	 */
+	public function get_settings() {
+		$settings = parent::get_settings();
+
+		$settings[] = 'mollie';
+
+		return $settings;
 	}
 }
