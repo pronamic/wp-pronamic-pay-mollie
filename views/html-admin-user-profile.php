9ce124523c648e65249a7e4eafc79e6d65d29d39
I <info@remcotolsma.nl>
2016-09-09 15:01:49 +0200

    Changed label of customer ID (live) (test).

--- views/html-admin-user-profile.php
+++ views/html-admin-user-profile.php
@@ -7,7 +7,7 @@
  * @see https://github.com/WordPress/WordPress/blob/4.5.2/wp-admin/user-edit.php#L578-L600
  */
 
-$customer_id      = get_user_meta( $user->ID, '_pronamic_pay_mollie_customer_id', true );
+$customer_id_live = get_user_meta( $user->ID, '_pronamic_pay_mollie_customer_id', true );
 $customer_id_test = get_user_meta( $user->ID, '_pronamic_pay_mollie_customer_id_test', true );
 
 ?>
@@ -16,18 +16,18 @@ $customer_id_test = get_user_meta( $user->ID, '_pronamic_pay_mollie_customer_id_
 <table class="form-table">
 	<tr>
 		<th>
-			<label for="pronamic_pay_mollie_customer_id">
-				<?php esc_html_e( 'Customer ID', 'pronamic_ideal' ); ?>
+			<label for="pronamic_pay_mollie_customer_id_live">
+				<?php esc_html_e( 'Customer ID (live)', 'pronamic_ideal' ); ?>
 			</label>
 		</th>
 		<td>
-			<input id="pronamic_pay_mollie_customer_id" name="pronamic_pay_mollie_customer_id" type="text" value="<?php echo esc_attr( $customer_id ); ?>" class="regular-text" readonly="readonly" />
+			<input id="pronamic_pay_mollie_customer_id_live" name="pronamic_pay_mollie_customer_id_live" type="text" value="<?php echo esc_attr( $customer_id_live ); ?>" class="regular-text" readonly="readonly" />
 		</td>
 	</tr>
 	<tr>
 		<th>
 			<label for="pronamic_pay_mollie_customer_id_test">
-				<?php esc_html_e( 'Test Customer ID', 'pronamic_ideal' ); ?>
+				<?php esc_html_e( 'Customer ID (test)', 'pronamic_ideal' ); ?>
 			</label>
 		</th>
 		<td>
