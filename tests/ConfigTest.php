7df32373e2d1b5bed3e57f0b0b4d8e0b825c5833
Remco Tolsma <info@remcotolsma.nl>
2017-01-25 11:24:56 +0100

    Updated version numbers.

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -7,7 +7,7 @@
  * Company: Pronamic
  *
  * @author Remco Tolsma
- * @version 1.1.9
+ * @version 1.1.11
  * @since 1.0.0
  */
 class Pronamic_WP_Pay_Mollie_ConfigTest extends PHPUnit_Framework_TestCase {
