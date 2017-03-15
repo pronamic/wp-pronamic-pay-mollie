706678e39f3e7cc5e07d24073e77a0ed67cd36f3
Remco Tolsma <info@remcotolsma.nl>
2017-03-15 12:24:46 +0100

    Getting ready for version 1.1.13.

--- src/Gateway.php
+++ src/Gateway.php
@@ -7,7 +7,7 @@
  * Company: Pronamic
  *
  * @author Remco Tolsma
- * @version 1.1.11
+ * @version 1.1.13
  * @since 1.1.0
  */
 class Pronamic_WP_Pay_Gateways_Mollie_Gateway extends Pronamic_WP_Pay_Gateway {
