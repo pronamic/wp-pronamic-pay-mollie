340b07ad634c694590de17d424c2fe9e02edeafc
Reüel van der Steege <reuel@pronamic.nl>
2019-02-09 03:14:53 +0100

    Update libraries, coding standards, tests and Travis config.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,9 +8,6 @@
  * @package   Pronamic\WordPress\Pay\Gateways\Mollie
  */
 
-/**
- * Autoload.
- */
 require_once __DIR__ . '/../vendor/autoload.php';
 
 require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/functions.php';
@@ -26,7 +23,5 @@ function _manually_load_plugin() {
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-/**
- * Bootstrap.
- */
+// Bootstrap.
 require getenv( 'WP_PHPUNIT__DIR' ) . '/includes/bootstrap.php';
