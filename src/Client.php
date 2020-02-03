cff3220862649141236cb748f8b423dbfbfed6c7
Reüel van der Steege <reuel@pronamic.nl>
2020-02-03 14:19:07 +0100

    Coding standards.

--- src/Client.php
+++ src/Client.php
@@ -260,6 +260,7 @@ class Client {
 	 *
 	 * @return null|object
 	 * @throws \InvalidArgumentException Throws exception on empty customer ID argument.
+	 * @throws \Exception Throws exception when request fails.
 	 */
 	public function get_customer( $customer_id ) {
 		if ( empty( $customer_id ) ) {
