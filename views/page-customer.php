1b631a6e4b6f7bd32de68da835146d90e66419f7
Remco Tolsma <info@remcotolsma.nl>
2020-03-11 16:11:49 +0100

    Check if Mollie customer can be found.

--- views/page-customer.php
+++ views/page-customer.php
@@ -10,11 +10,11 @@
 
 namespace Pronamic\WordPress\Pay\Gateways\Mollie;
 
-$mollie_customer_id = \filter_input( INPUT_GET, 'id', FILTER_SANITIZE_STRING );
-
 global $wpdb;
 
-$data = $wpdb->get_row(
+$mollie_customer_id = \filter_input( INPUT_GET, 'id', FILTER_SANITIZE_STRING );
+
+$mollie_customer_data = $wpdb->get_row(
 	$wpdb->prepare(
 		"
 		SELECT
@@ -22,7 +22,7 @@ $data = $wpdb->get_row(
 			IF ( mollie_customer.test_mode, mollie_profile.api_key_test, mollie_profile.api_key_live ) AS api_key
 		FROM
 			$wpdb->pronamic_pay_mollie_customers AS mollie_customer
-				INNER JOIN
+				LEFT JOIN
 			$wpdb->pronamic_pay_mollie_profiles AS mollie_profile
 					ON mollie_customer.profile_id = mollie_profile.id
 		WHERE
@@ -35,23 +35,29 @@ $data = $wpdb->get_row(
 	)
 );
 
-$client = new Client( $data->api_key );
+$mollie_customer = null;
 
-/**
- * Customer.
- *
- * @link https://docs.mollie.com/reference/v2/customers-api/get-customer
- */
-$customer = $client->get_customer( $mollie_customer_id );
+$mollie_customer_mandates = null;
 
-/**
- * Mandates.
- *
- * @link https://docs.mollie.com/reference/v2/mandates-api/list-mandates
- */
-$response = $client->get_mandates( $mollie_customer_id );
+if ( $mollie_customer_data->api_key ) {
+	$client = new Client( $mollie_customer_data->api_key );
 
-$mandates = $response->_embedded->mandates;
+	/**
+	 * Customer.
+	 *
+	 * @link https://docs.mollie.com/reference/v2/customers-api/get-customer
+	 */
+	$mollie_customer = $client->get_customer( $mollie_customer_id );
+
+	/**
+	 * Mandates.
+	 *
+	 * @link https://docs.mollie.com/reference/v2/mandates-api/list-mandates
+	 */
+	$response = $client->get_mandates( $mollie_customer_id );
+
+	$mollie_customer_mandates = $response->_embedded->mandates;
+}
 
 /**
  * WordPress user.
@@ -70,7 +76,7 @@ $users = $wpdb->get_results(
 			mollie_customer_user.customer_id = %d
 		;
 		",
-		$data->id
+		$mollie_customer_data->id
 	)
 );
 
@@ -103,36 +109,19 @@ $users = $wpdb->get_results(
 			<tr>
 				<th scope="row"><?php \esc_html_e( 'ID', 'pronamic_ideal' ); ?></th>
 				<td>
-					<code><?php echo \esc_html( $customer->id ); ?></code>
+					<code><?php echo \esc_html( $mollie_customer_data->id ); ?></code>
 				</td>
 			</tr>
 			<tr>
 				<th scope="row"><?php \esc_html_e( 'Mode', 'pronamic_ideal' ); ?></th>
 				<td>
-					<?php
-
-					switch ( $customer->mode ) {
-						case 'test':
-							\esc_html_e( 'Test', 'pronamic_ideal' );
-
-							break;
-						case 'live':
-							\esc_html_e( 'Live', 'pronamic_ideal' );
-
-							break;
-						default:
-							echo \esc_html( $customer->mode );
-
-							break;
-					}
-
-					?>
+					<?php $mollie_customer_data->test_mode ? \esc_html_e( 'Test', 'pronamic_ideal' ) : \esc_html_e( 'Live', 'pronamic_ideal' ); ?>
 				</td>
 			</tr>
 			<tr>
 				<th scope="row"><?php \esc_html_e( 'Name', 'pronamic_ideal' ); ?></th>
 				<td>
-					<?php echo \esc_html( $customer->name ); ?>
+					<?php echo \esc_html( $mollie_customer_data->name ); ?>
 				</td>
 			</tr>
 			<tr>
@@ -140,32 +129,38 @@ $users = $wpdb->get_results(
 				<td>
 					<?php
 
-					if ( null !== $customer->email ) {
+					if ( null !== $mollie_customer_data->email ) {
 						printf(
 							'<a href="%s">%s</a>',
-							esc_attr( 'mailto:' . $customer->email ),
-							esc_html( $customer->email )
+							esc_attr( 'mailto:' . $mollie_customer_data->email ),
+							esc_html( $mollie_customer_data->email )
 						);
 					}
 
 					?>
 				</td>
 			</tr>
-			<tr>
-				<th scope="row"><?php \esc_html_e( 'Locale', 'pronamic_ideal' ); ?></th>
-				<td>
-					<?php
 
-					if ( null !== $customer->locale ) {
-						printf(
-							'<code>%s</code>',
-							esc_html( $customer->locale )
-						);
-					}
+			<?php if ( null !== $mollie_customer ) : ?>
 
-					?>
-				</td>
-			</tr>
+				<tr>
+					<th scope="row"><?php \esc_html_e( 'Locale', 'pronamic_ideal' ); ?></th>
+					<td>
+						<?php
+
+						if ( null !== $mollie_customer->locale ) {
+							printf(
+								'<code>%s</code>',
+								esc_html( $mollie_customer->locale )
+							);
+						}
+
+						?>
+					</td>
+				</tr>
+
+			<?php endif; ?>
+
 			<tr>
 				<th scope="row"><?php \esc_html_e( 'Link', 'pronamic_ideal' ); ?></th>
 				<td>
@@ -173,7 +168,7 @@ $users = $wpdb->get_results(
 
 					$mollie_link = \sprintf(
 						'https://www.mollie.com/dashboard/customers/%s',
-						$customer->id
+						$mollie_customer_id
 					);
 
 					\printf(
@@ -188,227 +183,231 @@ $users = $wpdb->get_results(
 		</tbody>
 	</table>
 
-	<h3><?php \esc_html_e( 'Mandates', 'pronamic_ideal' ); ?></h3>
+	<?php if ( null !== $mollie_customer_mandates ) : ?>
 
-	<table class="widefat">
-		<thead>
-			<tr>
-				<th><?php \esc_html_e( 'ID', 'pronamic_ideal' ); ?></th>
-				<th><?php \esc_html_e( 'Mode', 'pronamic_ideal' ); ?></th>
-				<th><?php \esc_html_e( 'Status', 'pronamic_ideal' ); ?></th>
-				<th><?php \esc_html_e( 'Method', 'pronamic_ideal' ); ?></th>
-				<th><?php \esc_html_e( 'Details', 'pronamic_ideal' ); ?></th>
-				<th><?php \esc_html_e( 'Mandate Reference', 'pronamic_ideal' ); ?></th>
-				<th><?php \esc_html_e( 'Signature Date', 'pronamic_ideal' ); ?></th>
-				<th><?php \esc_html_e( 'Created On', 'pronamic_ideal' ); ?></th>
-			</tr>
-		</thead>
+		<h3><?php \esc_html_e( 'Mandates', 'pronamic_ideal' ); ?></h3>
 
-		<tbody>
-
-			<?php if ( empty( $mandates ) ) : ?>
-
+		<table class="widefat">
+			<thead>
 				<tr>
-					<td colspan="4"><?php esc_html_e( 'No mandates found.', 'pronamic_ideal' ); ?></td>
+					<th><?php \esc_html_e( 'ID', 'pronamic_ideal' ); ?></th>
+					<th><?php \esc_html_e( 'Mode', 'pronamic_ideal' ); ?></th>
+					<th><?php \esc_html_e( 'Status', 'pronamic_ideal' ); ?></th>
+					<th><?php \esc_html_e( 'Method', 'pronamic_ideal' ); ?></th>
+					<th><?php \esc_html_e( 'Details', 'pronamic_ideal' ); ?></th>
+					<th><?php \esc_html_e( 'Mandate Reference', 'pronamic_ideal' ); ?></th>
+					<th><?php \esc_html_e( 'Signature Date', 'pronamic_ideal' ); ?></th>
+					<th><?php \esc_html_e( 'Created On', 'pronamic_ideal' ); ?></th>
 				</tr>
+			</thead>
 
-			<?php else : ?>
+			<tbody>
 
-				<?php foreach ( $mandates as $mandate ) : ?>
+				<?php if ( empty( $mollie_customer_mandates ) ) : ?>
 
 					<tr>
-						<td>
-							<code><?php echo \esc_html( $mandate->id ); ?></code>
-						</td>
-						<td>
-							<?php
-
-							switch ( $mandate->mode ) {
-								case 'test':
-									\esc_html_e( 'Test', 'pronamic_ideal' );
-
-									break;
-								case 'live':
-									\esc_html_e( 'Live', 'pronamic_ideal' );
-
-									break;
-								default:
-									echo \esc_html( $mandate->mode );
-
-									break;
-							}
-
-							?>
-						</td>
-						<td>
-							<?php
-
-							switch ( $mandate->status ) {
-								case 'pending':
-									\esc_html_e( 'Pending', 'pronamic_ideal' );
-
-									break;
-								case 'valid':
-									\esc_html_e( 'Valid', 'pronamic_ideal' );
-
-									break;
-								default:
-									echo \esc_html( $mandate->status );
-
-									break;
-							}
-
-							?>
-						</td>
-						<td>
-							<?php
-
-							switch ( $mandate->method ) {
-								case 'creditcard':
-									\esc_html_e( 'Credit Card', 'pronamic_ideal' );
-
-									break;
-								case 'directdebit':
-									\esc_html_e( 'Direct Debit', 'pronamic_ideal' );
-
-									break;
-								default:
-									echo \esc_html( $mandate->method );
-
-									break;
-							}
-
-							?>
-						</td>
-						<td>
-							<?php
-
-							switch ( $mandate->method ) {
-								case 'creditcard':
-									?>
-									<dl style="margin: 0;">
-
-										<?php if ( ! empty( $mandate->details->cardHolder ) ) : ?>
-
-											<dt><?php \esc_html_e( 'Card Holder', 'pronamic_ideal' ); ?></dt>
-											<dd>
-												<?php echo \esc_html( $mandate->details->cardHolder ); ?>
-											</dd>
-
-										<?php endif; ?>
-
-										<?php if ( ! empty( $mandate->details->cardNumber ) ) : ?>
-
-											<dt><?php \esc_html_e( 'Card Number', 'pronamic_ideal' ); ?></dt>
-											<dd>
-												<?php echo \esc_html( $mandate->details->cardNumber ); ?>
-											</dd>
-
-										<?php endif; ?>
-
-										<?php if ( ! empty( $mandate->details->cardLabel ) ) : ?>
-
-											<dt><?php \esc_html_e( 'Card Label', 'pronamic_ideal' ); ?></dt>
-											<dd>
-												<?php echo \esc_html( $mandate->details->cardLabel ); ?>
-											</dd>
-
-										<?php endif; ?>
-
-										<?php if ( ! empty( $mandate->details->cardFingerprint ) ) : ?>
-
-											<dt><?php \esc_html_e( 'Card Fingerprint', 'pronamic_ideal' ); ?></dt>
-											<dd>
-												<?php echo \esc_html( $mandate->details->cardFingerprint ); ?>
-											</dd>
-
-										<?php endif; ?>
-
-										<?php if ( ! empty( $mandate->details->cardExpiryDate ) ) : ?>
-
-											<dt><?php \esc_html_e( 'Card Expiry Date', 'pronamic_ideal' ); ?></dt>
-											<dd>
-												<?php echo \esc_html( $mandate->details->cardExpiryDate ); ?>
-											</dd>
-
-										<?php endif; ?>
-									</dl>
-									<?php
-
-									break;
-								case 'directdebit':
-									?>
-									<dl style="margin: 0;">
-
-										<?php if ( ! empty( $mandate->details->consumerName ) ) : ?>
-
-											<dt><?php \esc_html_e( 'Consumer Name', 'pronamic_ideal' ); ?></dt>
-											<dd>
-												<?php echo \esc_html( $mandate->details->consumerName ); ?>
-											</dd>
-
-										<?php endif; ?>
-
-										<?php if ( ! empty( $mandate->details->consumerAccount ) ) : ?>
-
-											<dt><?php \esc_html_e( 'Consumer Account', 'pronamic_ideal' ); ?></dt>
-											<dd>
-												<?php echo \esc_html( $mandate->details->consumerAccount ); ?>
-											</dd>
-
-										<?php endif; ?>
-
-										<?php if ( ! empty( $mandate->details->consumerBic ) ) : ?>
-
-											<dt><?php \esc_html_e( 'Consumer BIC', 'pronamic_ideal' ); ?></dt>
-											<dd>
-												<?php echo \esc_html( $mandate->details->consumerBic ); ?>
-											</dd>
-
-										<?php endif; ?>
-									</dl>
-									<?php
-
-									break;
-								default:
-									?>
-									<pre><?php echo \esc_html( \wp_json_encode( $mandate->details, \JSON_PRETTY_PRINT ) ); ?></pre>
-									<?php
-
-									break;
-							}
-
-							?>
-						</td>
-						<td>
-							<?php echo \esc_html( $mandate->mandateReference ); ?>
-						</td>
-						<td>
-							<?php
-
-							$signature_date = new \DateTime( $mandate->signatureDate );
-
-							echo \esc_html( $signature_date->format( 'd-m-Y' ) );
-
-							?>
-						</td>
-						<td>
-							<?php
-
-							$created_on = new \DateTime( $mandate->createdAt );
-
-							echo \esc_html( $created_on->format( 'd-m-Y H:i:s' ) );
-
-							?>
-						</td>
+						<td colspan="4"><?php esc_html_e( 'No mandates found.', 'pronamic_ideal' ); ?></td>
 					</tr>
 
-				<?php endforeach; ?>
+				<?php else : ?>
 
-			<?php endif; ?>
+					<?php foreach ( $mollie_customer_mandates as $mandate ) : ?>
 
-		</tbody>
-	</table>
+						<tr>
+							<td>
+								<code><?php echo \esc_html( $mandate->id ); ?></code>
+							</td>
+							<td>
+								<?php
+
+								switch ( $mandate->mode ) {
+									case 'test':
+										\esc_html_e( 'Test', 'pronamic_ideal' );
+
+										break;
+									case 'live':
+										\esc_html_e( 'Live', 'pronamic_ideal' );
+
+										break;
+									default:
+										echo \esc_html( $mandate->mode );
+
+										break;
+								}
+
+								?>
+							</td>
+							<td>
+								<?php
+
+								switch ( $mandate->status ) {
+									case 'pending':
+										\esc_html_e( 'Pending', 'pronamic_ideal' );
+
+										break;
+									case 'valid':
+										\esc_html_e( 'Valid', 'pronamic_ideal' );
+
+										break;
+									default:
+										echo \esc_html( $mandate->status );
+
+										break;
+								}
+
+								?>
+							</td>
+							<td>
+								<?php
+
+								switch ( $mandate->method ) {
+									case 'creditcard':
+										\esc_html_e( 'Credit Card', 'pronamic_ideal' );
+
+										break;
+									case 'directdebit':
+										\esc_html_e( 'Direct Debit', 'pronamic_ideal' );
+
+										break;
+									default:
+										echo \esc_html( $mandate->method );
+
+										break;
+								}
+
+								?>
+							</td>
+							<td>
+								<?php
+
+								switch ( $mandate->method ) {
+									case 'creditcard':
+										?>
+										<dl style="margin: 0;">
+
+											<?php if ( ! empty( $mandate->details->cardHolder ) ) : ?>
+
+												<dt><?php \esc_html_e( 'Card Holder', 'pronamic_ideal' ); ?></dt>
+												<dd>
+													<?php echo \esc_html( $mandate->details->cardHolder ); ?>
+												</dd>
+
+											<?php endif; ?>
+
+											<?php if ( ! empty( $mandate->details->cardNumber ) ) : ?>
+
+												<dt><?php \esc_html_e( 'Card Number', 'pronamic_ideal' ); ?></dt>
+												<dd>
+													<?php echo \esc_html( $mandate->details->cardNumber ); ?>
+												</dd>
+
+											<?php endif; ?>
+
+											<?php if ( ! empty( $mandate->details->cardLabel ) ) : ?>
+
+												<dt><?php \esc_html_e( 'Card Label', 'pronamic_ideal' ); ?></dt>
+												<dd>
+													<?php echo \esc_html( $mandate->details->cardLabel ); ?>
+												</dd>
+
+											<?php endif; ?>
+
+											<?php if ( ! empty( $mandate->details->cardFingerprint ) ) : ?>
+
+												<dt><?php \esc_html_e( 'Card Fingerprint', 'pronamic_ideal' ); ?></dt>
+												<dd>
+													<?php echo \esc_html( $mandate->details->cardFingerprint ); ?>
+												</dd>
+
+											<?php endif; ?>
+
+											<?php if ( ! empty( $mandate->details->cardExpiryDate ) ) : ?>
+
+												<dt><?php \esc_html_e( 'Card Expiry Date', 'pronamic_ideal' ); ?></dt>
+												<dd>
+													<?php echo \esc_html( $mandate->details->cardExpiryDate ); ?>
+												</dd>
+
+											<?php endif; ?>
+										</dl>
+										<?php
+
+										break;
+									case 'directdebit':
+										?>
+										<dl style="margin: 0;">
+
+											<?php if ( ! empty( $mandate->details->consumerName ) ) : ?>
+
+												<dt><?php \esc_html_e( 'Consumer Name', 'pronamic_ideal' ); ?></dt>
+												<dd>
+													<?php echo \esc_html( $mandate->details->consumerName ); ?>
+												</dd>
+
+											<?php endif; ?>
+
+											<?php if ( ! empty( $mandate->details->consumerAccount ) ) : ?>
+
+												<dt><?php \esc_html_e( 'Consumer Account', 'pronamic_ideal' ); ?></dt>
+												<dd>
+													<?php echo \esc_html( $mandate->details->consumerAccount ); ?>
+												</dd>
+
+											<?php endif; ?>
+
+											<?php if ( ! empty( $mandate->details->consumerBic ) ) : ?>
+
+												<dt><?php \esc_html_e( 'Consumer BIC', 'pronamic_ideal' ); ?></dt>
+												<dd>
+													<?php echo \esc_html( $mandate->details->consumerBic ); ?>
+												</dd>
+
+											<?php endif; ?>
+										</dl>
+										<?php
+
+										break;
+									default:
+										?>
+										<pre><?php echo \esc_html( \wp_json_encode( $mandate->details, \JSON_PRETTY_PRINT ) ); ?></pre>
+										<?php
+
+										break;
+								}
+
+								?>
+							</td>
+							<td>
+								<?php echo \esc_html( $mandate->mandateReference ); ?>
+							</td>
+							<td>
+								<?php
+
+								$signature_date = new \DateTime( $mandate->signatureDate );
+
+								echo \esc_html( $signature_date->format( 'd-m-Y' ) );
+
+								?>
+							</td>
+							<td>
+								<?php
+
+								$created_on = new \DateTime( $mandate->createdAt );
+
+								echo \esc_html( $created_on->format( 'd-m-Y H:i:s' ) );
+
+								?>
+							</td>
+						</tr>
+
+					<?php endforeach; ?>
+
+				<?php endif; ?>
+
+			</tbody>
+		</table>
+
+	<?php endif; ?>
 
 	<h3><?php \esc_html_e( 'WordPress Users', 'pronamic_ideal' ); ?></h3>
 
