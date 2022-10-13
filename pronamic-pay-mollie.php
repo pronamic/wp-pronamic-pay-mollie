8584083137dcdbdda7c3e93a37e196ef0eb30174
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2022-10-13 14:55:25 +0200

    Create pronamic-pay-mollie.php

---
+++ pronamic-pay-mollie.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: Pronamic Pay Mollie Add-On
+ * Plugin URI: https://www.pronamic.eu/plugins/pronamic-pay-mollie/
+ * Description: Extend the Pronamic Pay plugin with the Mollie gateway to receive payments through a variety of WordPress plugins.
+ *
+ * Version: 4.2.2
+ * Requires at least: 4.7
+ *
+ * Author: Pronamic
+ * Author URI: https://www.pronamic.eu/
+ *
+ * Text Domain: pronamic-pay-mollie
+ * Domain Path: /languages/
+ *
+ * License: GPL-3.0-or-later
+ *
+ * Depends: wp-pay/core
+ *
+ * GitHub URI: https://github.com/pronamic/wp-pronamic-pay-mollie
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2022 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Pay\Gateways\Mollie
+ */
