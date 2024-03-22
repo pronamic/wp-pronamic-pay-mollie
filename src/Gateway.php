8ff505442e2bf788b039ce061fde553fcdc5d459
Reüel van der Steege <reuel@pronamic.nl>
2024-03-22 11:32:43 +0100

    Fix unnecessary subscription payment method updates with cards.

--- src/Gateway.php
+++ src/Gateway.php
@@ -1623,7 +1623,19 @@ class Gateway extends Core_Gateway {
 		$method_transformer = new MethodTransformer();
 
 		$old_method = $subscription->get_payment_method();
-		$new_method = ( null === $payment_method && \property_exists( $mandate, 'method' ) ? $method_transformer->transform_mollie_to_wp( $mandate->method ) : $payment_method );
+		$new_method = $payment_method;
+
+		if ( null === $payment_method && \property_exists( $mandate, 'method' ) ) {
+			$pronamic_methods = $method_transformer->from_mollie_to_pronamic( $mandate->method );
+
+			$new_method = in_array( $old_method, $pronamic_methods, true ) ? $old_method : null;
+
+			$first_pronamic_method = reset( $pronamic_methods );
+
+			if ( null === $new_method && false !== $first_pronamic_method ) {
+				$new_method = $first_pronamic_method;
+			}
+		}
 
 		if ( ! empty( $old_method ) && $old_method !== $new_method ) {
 			$subscription->set_payment_method( $new_method );
