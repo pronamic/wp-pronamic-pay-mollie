9fc90e00f2c4938bc6b4f79629ba8bbe51f28319
Remco Tolsma <remcotolsma@pronamic.nl>
2025-03-28 10:43:41 +0100

    Create rector.php

---
+++ rector.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Rector
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2025 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Pay
+ */
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+	->withPaths(
+		[
+			__DIR__ . '/src',
+			__DIR__ . '/tests',
+			__DIR__ . '/views',
+		]
+	)
+	// uncomment to reach your current PHP version
+	// ->withPhpSets()
+	->withTypeCoverageLevel( 0 );
